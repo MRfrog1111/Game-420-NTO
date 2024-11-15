45345cfacaed1dfe0742f3b2feff65f689d4025f
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2024-11-15 23:26:35 +0700

    начало ресурсов

---
+++ NTO420/Assets/Scripts/MC_inGameInformation.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_inGameInformation : MonoBehaviour
+{
+    public int hp;
+    public int oxygen;
+    public int hunger;
+}
