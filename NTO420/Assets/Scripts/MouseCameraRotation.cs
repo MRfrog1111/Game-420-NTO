47e5f3a78b5b69e3c1fdb32c5edf29b463910d64
NikUse <abramownikita07@gmail.com>
2024-11-18 21:57:46 +0300

    novoe dvizenie
    nemnogo peredelal upravlenie

--- NTO420/Assets/Scripts/MouseCameraRotation.cs
+++ NTO420/Assets/Scripts/MouseCameraRotation.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class MouseCameraRotation : MonoBehaviour
 {
-    public Vector2 turn;
-
+    
     public float sensetivity;
 
-    public GameObject player;
+    public Transform player;
 
-    public GameObject pos;
-    // Start is called before the first frame update
-    void Start()
+    private float xRotation;
+    private float yRotation;
+    private float x;
+    
+
+    private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        turn.x += Input.GetAxis("Mouse X")*Time.deltaTime *sensetivity;
-        turn.y += Input.GetAxis("Mouse Y") * sensetivity*Time.deltaTime;
-        transform.localRotation = Quaternion.Euler(-turn.y,turn.x,0);
-        player.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
-        transform.position = pos.transform.position;
+        yRotation = Input.GetAxis("Mouse Y") * sensetivity * Time.deltaTime;
+        xRotation = Input.GetAxis("Mouse X") * sensetivity * Time.deltaTime;
+
+        x -= yRotation;
+        x = Mathf.Clamp(x, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(x, 0f, 0f);
+        player.Rotate(Vector3.up * xRotation);
+        
     }   
     
 }
