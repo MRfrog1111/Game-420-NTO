8451c12bb0fa8004120ebeafb02a5063bc63067b
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2024-11-20 22:06:05 +0700

    комменты

---
+++ NTO420/Assets/Scripts/WebWork/Structures/ChangesLogStructure.cs
@@ -0,0 +1,9 @@
+namespace DefaultNamespace
+{
+    public struct PlayerChangesLogs
+    {
+        public string hp_change; 
+        public string oxygen_change;
+        public string food_change;
+    }
+}
---
+++ NTO420/Assets/Scripts/WebWork/Structures/LogStructure.cs
@@ -0,0 +1,9 @@
+namespace DefaultNamespace
+{
+    public struct PlayerLogs
+    {
+        public string comment; 
+        public string player_name;
+        public string resources_changed;
+    }
+}
