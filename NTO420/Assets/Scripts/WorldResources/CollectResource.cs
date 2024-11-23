d5e2ef159d1b5866243dc759644c359a7269dc05
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2024-11-23 18:44:47 +0700

    Сбор ресурсов

--- NTO420/Assets/Scripts/WorldResources/CollectResource.cs
+++ NTO420/Assets/Scripts/WorldResources/CollectResource.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DefaultNamespace;
 public class CollectResource : MonoBehaviour
 {
+    [SerializeField] private PlayerRequests webAsker;
+    private PlayerResources resources;
     [SerializeField] private LayerMask pickableLayerMask;
-
     [SerializeField] private Transform playerCameraTransform;
     [SerializeField] private GameObject pickapUI;
     private float hitRange = 3;
@@ -28,10 +29,24 @@ public class CollectResource : MonoBehaviour
             
         }
     }
-
+    public void GetRes(PlayerResources res)
+    {
+        resources = res;
+    }
     void AddResource(GameObject obj)
     {
-        print(obj.name);
+        StartCoroutine(webAsker.GetPlayerResources(GetRes));
+        switch (obj.name)
+        {
+            case "Honey":
+                resources.honey += 1;
+                break;
+            default:
+                print("there's no such resource");
+                break;
+        }
+        StartCoroutine(webAsker.UpdatePlayerResources(resources));
+        StartCoroutine(webAsker.GetPlayerResources(GetRes));
         Destroy(obj);
     }
     
