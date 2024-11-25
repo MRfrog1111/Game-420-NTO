5dca55223d28bf8168dbdfad68a1782011feb1ce
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2024-11-25 21:37:19 +0700

    Работающий лог еды + ресурсы

--- NTO420/Assets/Scripts/WebWork/Structures/ChangesLogStructure.cs
+++ NTO420/Assets/Scripts/WebWork/Structures/ChangesLogStructure.cs
@@ -1,6 +1,7 @@
 namespace DefaultNamespace
 {
-    public struct PChangeLogs
+    [System.Serializable]
+    public struct PlayerChangesLogs
     {
         public string hp_change; 
         public string oxygen_change;
