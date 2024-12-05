eaefd7ca4323e206ec2d98e2e0740968b51f0050
NikUse <abramownikita07@gmail.com>
2024-12-05 22:03:26 +0300

    прыжок

--- NTO420/Assets/Scripts/Player/Controller.cs
+++ NTO420/Assets/Scripts/Player/Controller.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float speed = 5f;
+    public float gravity = 5f;
+
+
+    public int jumpForse = 2;
     private int run = 1;
+
+    private Vector3 velocity;
 
     private PlayerResources resources;
     public CharacterController characterController;
@@ -21,8 +27,26 @@ public class Controller : MonoBehaviour
         else run = 1;
         characterController.Move(move * speed * run * Time.deltaTime);
 
+        if (Input.GetKey(KeyCode.Space) && characterController.isGrounded)
+            Jump();
+
+    }
+    private void FixedUpdate()
+    {
+        Gravity(characterController.isGrounded);
     }
 
-    
+    private void Gravity(bool isGrounded)
+    {
+        if(isGrounded && velocity.y < 0)
+            velocity.y = -1f;
+        velocity.y -= gravity * Time.fixedDeltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = jumpForse;
+    }
 
 }
