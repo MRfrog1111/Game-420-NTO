7334d7a884f07ced3362b672898c674ad6eb2af0
NikUse <abramownikita07@gmail.com>
2024-12-07 22:22:37 +0300

    Settings

--- NTO420/Assets/Scripts/MenuManager.cs
+++ NTO420/Assets/Scripts/MenuManager.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class MenuManager : MonoBehaviour
 {
     static float soundVolume;
     static float sensetivityVolume;
-    static bool Start = false;
+    static bool _Start = false;
+
+    Resolution[] resolutions;
+    List<string> options = new List<string>();
 
     public GameObject Settings;
     public GameObject MeinMenu;
     public Slider volume;
     public Slider sensetivity;
     public MouseLook mouseLook;
+    public TMP_Text _resolutions;
+
+    int currentResolutionIndex = 0;
 
     private void Awake()
     {
         Settings.SetActive(false);
-        if (Start)
+        if (_Start)
         {
             AudioListener.volume = soundVolume;
             volume.value = soundVolume;
@@ -29,6 +36,34 @@ public class MenuManager : MonoBehaviour
             mouseLook.rotationSpeed = sensetivityVolume;
             sensetivity.value = sensetivityVolume;
         }
+    }
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        _resolutions.text = options[currentResolutionIndex].ToString();
+        Resolution resolution = resolutions[currentResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetResolution()
+    {
+        
     }
 
     public void PlayGame()
@@ -53,12 +88,28 @@ public class MenuManager : MonoBehaviour
     {
         AudioListener.volume = volume.value;
         soundVolume = volume.value;
-        Start = true;
+        _Start = true;
     }
 
     public void Sensetivity()
     {
         mouseLook.rotationSpeed *= sensetivity.value;
         sensetivityVolume = sensetivity.value;
+        _Start = true;
+    }
+
+    public void PlusIndex()
+    {
+        if(currentResolutionIndex++ < resolutions.Length)
+            currentResolutionIndex++;
+        else 
+            currentResolutionIndex = 0;
+    }
+    public void MinesIndex()
+    {
+        if(currentResolutionIndex-- > 0)
+            currentResolutionIndex--;
+        else
+            currentResolutionIndex = resolutions.Length;
     }
 }
