c53bf1e1de028755c5dcdde1ca145dd4a67256ee
NikUse <abramownikita07@gmail.com>
2024-12-08 01:37:43 +0300

    звук

---
+++ NTO420/Assets/пасека + генератор/Generator1.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+using static UnityEditor.Progress;
+using UnityEngine.UI;
+using TMPro;
+public class Generator1 : MonoBehaviour
+{
+    public Transform camera;
+    public LayerMask pickableLayerMask;
+    public CollectResource slots;
+    public PlayerStats stats;
+    public Tutorial tutor;
+    private float hitRange = 3f;
+    private RaycastHit hit;
+    public TextMeshProUGUI generatortext;
+    
+    public int MaxHoney = 100;
+    public int rashodHoney = 100;
+    public int timeInSecond = 100;
+    public int honeyNow;
+
+    public ManagerUI ui;
+    public Paseka paseka;
+    private void Start()
+    {
+        StartCoroutine(GeneratorRashod());
+    }
+
+    private void Update()
+    {
+        generatortext.text = honeyNow.ToString() + "%";
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHightLight(false);
+        }
+        if (Physics.Raycast(camera.position, camera.forward, out hit, hitRange, pickableLayerMask))
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHightLight(true);
+           if(Input.GetKey(KeyCode.E))
+            {
+                if (!tutor.isUsedGenerator)
+                {
+                    tutor.isUsedGenerator = true;
+                }
+
+                GeneratorOfHoney();
+            }
+        }
+        
+    }
+
+    private void GeneratorOfHoney()
+    {
+        foreach (SlotInventory slot in slots.slots)
+        {
+            if (slot.item.itemType == ItemType.Honey)
+            {
+                StartCoroutine(HoneyFull(slot));
+                if (slot.count == 0)
+                {
+                    //обновление слота
+                    slot.itemCountText.text = "";
+                    slot._icon.GetComponent<Image>().sprite = null;
+                    slot.item = null;
+                    slot._icon = null;
+                    slot.isEmpty = true;
+                    slot._icon = null;
+                    break;
+                }
+
+            }
+        }
+    }
+
+    private IEnumerator GeneratorRashod()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeInSecond);
+            if(honeyNow > 0)
+            {
+                honeyNow--;
+                if (!ui.isWorking)
+                {
+                    paseka.isWorking = true;
+                    ui.isWorking = true;
+                    stats.isWorking = true;
+                }
+
+                print(honeyNow);
+            }
+            else
+            {
+                if (ui.isWorking)
+                {
+                    paseka.isWorking = false;
+                    ui.isWorking = false;
+                    stats.isWorking = false;
+                }
+            }
+        }
+    }
+
+    private IEnumerator HoneyFull(SlotInventory _slot)
+    {
+        /*while(Input.GetKey(KeyCode.E) && _slot.count > 0)
+        {*/
+            yield return new WaitForSeconds(0.1f);
+            
+            if(_slot.count > 0 && honeyNow < MaxHoney)
+            {
+                _slot.count--;
+                _slot.itemCountText.text = _slot.count.ToString();
+                stats.resources.honey--;
+                honeyNow++;
+                stats.UpdateRes();
+            }
+        //}
+    }
+}
---
+++ NTO420/Assets/пасека + генератор/Paseka1.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+//using static UnityEditor.Progress;
+
+public class Paseka : MonoBehaviour
+{
+    public int maxHoney = 100;
+    public int HoneyInSecond = 5;
+    public int HoneyNow = 0;
+    public float _time = 5f;
+
+    public Transform Camera;
+    public LayerMask layerMask;
+    public Items Honey;
+    [SerializeField] private PlayerStats stats;
+    [SerializeField] private CollectResource slots;
+    [SerializeField] private Tutorial tutor;
+    private float hitRange = 3f;
+    RaycastHit hit;
+    public bool isWorking = true;
+    private void Start()
+    {
+        StartCoroutine(Pasek());
+        
+    }
+
+    private void Update()
+    {
+        Honey.count = HoneyNow;
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Highlight>()?.ToggleHightLight(false);
+        }
+        if (Physics.Raycast(Camera.position, Camera.forward, out hit, hitRange, layerMask))
+            {
+                hit.collider.GetComponent<Highlight>()?.ToggleHightLight(true);
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                TakeHoney(); 
+                }
+        }    
+    }
+
+    private IEnumerator Pasek()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_time);
+            if (isWorking)
+                if (HoneyNow < maxHoney)
+                    HoneyNow += HoneyInSecond;
+                else
+                    HoneyNow = 100;
+        }
+    }
+        //print("HoneyInPaseka " + HoneyNow);
+        
+    public void TakeHoney()
+    {
+        stats.CheckUpdates();
+        stats.resources.honey += HoneyNow;
+        stats.UpdateRes();
+        HoneyNow = 0;
+        tutor.isGotHoney = true;
+        tutor.CheckStage();
+        int temporary = 0;
+        foreach (SlotInventory slot in slots.slots)
+        {
+            if (slot.item == Honey)
+            {
+               
+                if (slot.count + HoneyNow <= Honey.Item.maxCount)
+                {
+                    slot.count += Honey.count;
+                    slot.itemCountText.text = slot.count.ToString();
+                    
+                }
+                else
+                {
+                    temporary = (slot.count + HoneyNow) - Honey.Item.maxCount;
+                    return;
+                    
+                }
+                break;
+            }
+        }
+
+        foreach (SlotInventory slot in slots.slots)
+        {
+            if (slot.isEmpty == true)
+            {
+                //print(_item.name + _count);
+                slot.item = Honey.Item;
+                slot.count = Honey.count;
+                slot.isEmpty = false;
+                slot.SetIcon(Honey.Item.icon);
+                slot.itemCountText.text = slot.count.ToString();
+                break;
+            }
+        }
+    }
+}
