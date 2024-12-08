80fa545246aae150a0af6f37fcadcc2b6223fdf4
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2024-12-08 23:56:16 +0700

    craft window

---
+++ NTO420/Assets/Scripts/UI/ShowCraftInformation.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowCraftInformation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject info;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        info.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        info.SetActive(false);
+    }
+}
