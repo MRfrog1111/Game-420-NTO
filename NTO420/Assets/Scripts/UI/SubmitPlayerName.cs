43cf9c100c121cce75bd2e4d2f6a527e1e38aace
NikUse <abramownikita07@gmail.com>
2024-12-08 17:35:46 +0300

    1

--- NTO420/Assets/Scripts/UI/SubmitPlayerName.cs
+++ NTO420/Assets/Scripts/UI/SubmitPlayerName.cs
@@ -14,6 +14,9 @@ public class SubmitPlayerName : MonoBehaviour
     [SerializeField] private ShopRequests shopReq;
     //[SerializeField] private TextMeshProUGUI textUI;
     [SerializeField] private GameObject inputField;
+    [SerializeField] private GameObject panel;
+    private Animator anim;
+
     private PlayersStruct players;
     private string inputName;
     private bool canCreate = true;
@@ -27,6 +30,10 @@ public class SubmitPlayerName : MonoBehaviour
     private void Awake()
     {
         StartCoroutine(webAsker.GetPlayers(GetPlrs));
+    }
+    private void Start()
+    {
+        anim = panel.GetComponent<Animator>();
     }
 
     public void SubmitName()
@@ -55,6 +62,8 @@ public class SubmitPlayerName : MonoBehaviour
         }
 
         PlayerPrefs.SetString("PlayerName",inputName);
+        anim.Play("Zatemnenie");
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
