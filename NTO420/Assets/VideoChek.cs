edbfe95b2cbc4e8b8d5f87c250321f57240d5836
NikUse <abramownikita07@gmail.com>
2024-12-08 16:17:16 +0300

    prolog

--- NTO420/Assets/VideoChek.cs
+++ NTO420/Assets/VideoChek.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class VideoChek : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject button;
+    public GameObject image;
+    
+    
+    
+    private void Start()
     {
         videoPlayer.Play();
+        button.SetActive(false);
+        image.SetActive(false);
+        StartCoroutine(ShowButton());
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        if (button != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            
+        }
+        
+    }
+    private IEnumerator ShowButton()
+    {
+        yield return new WaitForSeconds(120f / videoPlayer.playbackSpeed);
+        button.SetActive(true);
+        image.SetActive(true);
         
     }
 }
