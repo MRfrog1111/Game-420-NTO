8fc47e37f8aa9cb00035c230d9476d63ff7d5c74
NikUse <abramownikita07@gmail.com>
2025-01-22 17:35:57 +0300

    1

--- NTO420/Assets/Scripts/Resourses/Honey.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="Honey", menuName ="Inventory/Honey")]
-public class Honey : ItemScriptableObject
-{
-    public float healCount;
-}
--- NTO420/Assets/Scripts/Resourses/SiliconSand.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SiliconSand", menuName = "Inventory/SiliconSand")]
-public class SiliconSand : ItemScriptableObject
-{
-   
-}
