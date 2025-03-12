7e71a3c3a23dd05535629b1dd3d36974b8b3b2a4
NikUse <abramownikita07@gmail.com>
2025-03-12 18:02:25 +0300

    1

--- NTO420/Assets/Scripts/Player/CameraController.cs
+++ NTO420/Assets/Scripts/Player/CameraController.cs
@@ -7,9 +7,43 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float sensX;
     [SerializeField] private float sensY;
 
-    private Camera cam;
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform orientaition;
 
-    private float mouxeX;
-    private float mouxeY;
+    private float mouseX;
+    private float mouseY;
+
+    private float multiplayer = 0.01f;
+
+    private float xRotaition;
+    private float yRotaition;
+
+    private void Start()
+    {
+        
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        cam.transform.localRotation = Quaternion.Euler(xRotaition, yRotaition, 0);
+        orientaition.transform.rotation = Quaternion.Euler(0, yRotaition, 0);
+    }
+
+    private void MyInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotaition += mouseX * sensX * multiplayer;
+        xRotaition -= mouseY * sensY * multiplayer;
+
+        xRotaition = Mathf.Clamp(xRotaition, -90f, 90f);   
+    }
+
 
 }
--- NTO420/Assets/Scripts/Player/NewController.cs
+++ NTO420/Assets/Scripts/Player/NewController.cs
@@ -5,26 +5,62 @@ using UnityEngine;
 
 public class NewController : MonoBehaviour
 {
+
+
+    [Header("Player Meaning")]
     public float MoveSpeed = 6f;
-    public float movementMultiply = 10f;
+    public float jumpForce = 10f;
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float runSpeed = 4f;
+    [SerializeField] private float acceleration = 10f;
+
+    [Header("Other Things")]
+    [SerializeField] private float movementMultiply = 10f;
+    [SerializeField] private float airMultiply = 0.4f;
+    [SerializeField] private LayerMask layerGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] Transform orientaition;
 
     private float horzintalMovement;
     private float verticalMovement;
-    private float drag = 6f;
+
+
+    private float plauerHeight;
+    private float groundDistance = 0.4f;
+
+    private float groundDrag = 6f;
+    private float airDrag = 2f;
 
     private Vector3 moveDirection;
+    private Vector3 slopeMoveDirection;
+
+
     private Rigidbody rb;
+    private RaycastHit slopeHit;
+
+    private bool isGrounded;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        plauerHeight = transform.localScale.y;
     }
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, layerGround);
+
         MyInput();
         ControllDrag();
+        ControlSpeed();
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
     private void FixedUpdate()
@@ -32,22 +68,85 @@ public class NewController : MonoBehaviour
         MovePlayer();
     }
 
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, plauerHeight / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void Sit()
+    {
+        
+    }
+
     private void MyInput()
     {
         horzintalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        moveDirection = transform.forward * verticalMovement + transform.right * horzintalMovement;
+        moveDirection = orientaition.forward * verticalMovement + orientaition.right * horzintalMovement;
     }
 
     private void MovePlayer()
     {
-        rb.AddForce(moveDirection.normalized * MoveSpeed * movementMultiply, ForceMode.Acceleration);
+        if (isGrounded && !OnSlope())
+        {
+            rb.AddForce(moveDirection.normalized * MoveSpeed * movementMultiply, ForceMode.Acceleration);
+
+        }
+        else if(isGrounded && OnSlope())
+        { 
+            rb.AddForce(slopeMoveDirection.normalized * MoveSpeed * movementMultiply, ForceMode.Acceleration);
+
+        }
+        else if(!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * MoveSpeed * movementMultiply * airMultiply, ForceMode.Acceleration);
+
+        }
+    }
+
+    private void ControlSpeed()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            MoveSpeed = Mathf.Lerp(MoveSpeed, runSpeed, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            MoveSpeed = Mathf.Lerp(MoveSpeed, walkSpeed, acceleration * Time.deltaTime);
+
+        }
     }
 
     private void ControllDrag()
     {
-        rb.drag = drag;
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    
 
 }
