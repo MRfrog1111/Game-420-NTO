53de4c528e404bbcc3b12eff699f7058437c7c32
NikUse <abramownikita07@gmail.com>
2025-03-18 20:35:56 +0300

    вроде бы выровнял UI но не весь

--- NTO420/Assets/Scripts/UI/Settings/ChangeResolutions.cs
+++ NTO420/Assets/Scripts/UI/Settings/ChangeResolutions.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Progress;
+using UnityEngine.Rendering;
+
 
 public class ChangeResolutions : MonoBehaviour
 {
