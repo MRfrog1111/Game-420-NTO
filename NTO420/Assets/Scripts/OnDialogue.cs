6955563cb7e9042e377710b923e9c7d805b10d09
NikUse <abramownikita07@gmail.com>
2025-03-23 00:11:13 +0300

    почти готовый билд

---
+++ NTO420/Assets/Scripts/OnDialogue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class OnDialogue : MonoBehaviour
+{
+    [SerializeField] private NPCConversation conversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            ConversationManager.Instance.StartConversation(conversation);
+            StartCoroutine(DestroyTrigger());
+
+        }
+
+    }
+
+    private IEnumerator DestroyTrigger()
+    {
+        yield return new WaitForSeconds(15f);
+        transform.gameObject.SetActive(false);
+    }
+}
