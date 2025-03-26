16cf118863c2d8d6ba2739859b4ae94d0c93467c
NikUse <161535653+NikUse@users.noreply.github.com>
2025-03-26 09:33:34 +0300

    1

---
+++ NTO420/Assets/Scripts/Generation/GenerationTerrain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationTerrain : MonoBehaviour
+{
+    private Terrain terrain;
+
+    private void Start()
+    {
+        terrain = GetComponent<Terrain>();
+
+    }
+
+    private void Update()
+    {
+        terrain.
+    }
+}
