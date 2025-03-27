b66c61a383b78f57a66f5f736b2eee6b9e6e365d
NikUse <161535653+NikUse@users.noreply.github.com>
2025-03-27 14:32:06 +0300

    ngj

--- NTO420/Assets/LoadSeed.cs
+++ NTO420/Assets/LoadSeed.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 using System;
+using System.Collections;
 
 public class LoadSeed : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class LoadSeed : MonoBehaviour
     public HeightMapSettings ns;
     public InputField InputField;
 
+    public TMP_Text nezy;
+
+    private void Start()
+    {
+        nezy.gameObject.SetActive(false);
+    }
     public void RandomSeed()
     {
         ns.noiseSettings.seed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
@@ -20,7 +27,11 @@ public class LoadSeed : MonoBehaviour
     public void SpecialSeed()
     {
         if(Convert.ToInt32(InputField.text) > int.MaxValue && Convert.ToInt32(InputField.text) < int.MinValue)
+        {
+            nezy.gameObject.SetActive(true);
+            StartCoroutine(NezyOff());
             return;
+        }
         ns.noiseSettings.seed = Convert.ToInt32(InputField.text);
         Load();
     }
@@ -28,5 +39,11 @@ public class LoadSeed : MonoBehaviour
     public void Load()
     {
         SceneManager.LoadScene("Terrain");
+    }
+
+    public IEnumerator NezyOff()
+    {
+        yield return new WaitForSeconds(2f);
+        nezy.gameObject.SetActive(false);
     }
 }
